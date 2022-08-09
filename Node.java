cd34e9497ed8459d62c7e7d7a22702a8c2660cf9
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-08-09 00:40:02 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,23 @@
+class Node<E> {
+    public E data;
+    public Node<E> left;
+    public Node<E> right;
+    public int height;
+    public boolean color; // 0 - negro; 1 - rojo
+    public Node<E> parent;
+
+    // Constructores
+    public Node(E data, Node<E> left, Node<E> right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+        this.height = 0;
+        this.color = false;
+        this.parent = null;
+    }
+
+    public Node(E data) {
+        this(data, null, null);
+    }
+    // getters, setters
+}
